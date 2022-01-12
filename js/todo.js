ab1702c59a04008fc3483b477cb1999bd127ace9
IronPikachu <simon.pikachu@hotmail.se>
2022-01-12 15:34:47 +0100

    Probably only prettifying it more until it's done

--- js/todo.js
+++ js/todo.js
@@ -9,12 +9,22 @@ function addItem(){
         let list = document.getElementById('itemList');
         let newitem = document.createElement('li');
         let newitemid = 'li_number_' + li_item_index++;
+
+        let rmvbtn = document.createElement('button');
+        rmvbtn.innerHTML = 'Remove';
+        rmvbtn.classList.add('btn-danger');
+        rmvbtn.addEventListener('click', function(){
+            removeitem(newitemid);
+        });
     
         newitem.appendChild(document.createTextNode(item));
+        newitem.appendChild(rmvbtn);
         newitem.setAttribute('id', newitemid);
         newitem.addEventListener('click', function(){
             finish(newitemid);
         });
+
+        newitem.classList.add('list-group-item');
     
         list.appendChild(newitem);
     }
@@ -25,7 +35,9 @@ function addItem(){
 
 function undo(){
     if(latest_item_id.length > 0){
-        document.getElementById(latest_item_id.pop()).classList.remove('finito');
+        let elem = latest_item_id.pop();
+        document.getElementById(elem).classList.remove('active');
+        document.getElementById(elem).classList.remove('finito');
     }
     else{
         console.log('empty');
@@ -33,9 +45,23 @@ function undo(){
 }
 
 function finish(itemid){
+    document.getElementById(itemid).classList.add('active');
     document.getElementById(itemid).classList.add('finito');
 
     latest_item_id.push(itemid);
+}
+
+function removeitem(itemid){
+    let li = document.getElementById(itemid);
+    li.remove();
+    if(latest_item_id.includes(itemid)){
+        for(let i = 0; i < latest_item_id.length; i++){
+            if(latest_item_id[i] === itemid){
+                latest_item_id.splice(i, 1);
+                return;
+            }
+        }
+    }
 }
 
 document.getElementById('addItem').onclick = addItem;
